32941352918577efe1898d6176cbef0da260f6ab
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2023-09-13 03:41:01 +0530

    commit

--- lib/screens/clips/create_clips.dart
+++ lib/screens/clips/create_clips.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -25,6 +27,7 @@ class _CreateClipsState extends ConsumerState<CreateClips> {
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider);
+    Future<SelectedImagesDetails?> file;
     return currentUser.when(
       data: (data) {
         if (data == null) {
@@ -33,35 +36,46 @@ class _CreateClipsState extends ConsumerState<CreateClips> {
           );
         }
         return Scaffold(
+          backgroundColor: Colors.black,
           body: Center(
               child: SingleChildScrollView(
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Container(color: Colors.black, width: 200, height: 600),
+                Row(
+                  children: [
+                    Expanded(child: TextField()),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {
+                          Future<SelectedImagesDetails?> files =
+                              ImagePickerPlus(
+                            context,
+                          ).pickBoth(
+                                  source: ImageSource.both,
+                                  multiSelection: true,
+                                  galleryDisplaySettings:
+                                      GalleryDisplaySettings(
+                                    cropImage: true,
+                                    showImagePreview: true,
+                                    gridDelegate:
+                                        SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3,
+                                      crossAxisSpacing: 5,
+                                      mainAxisSpacing: 5,
+                                    ),
+                                  ));
+                          setState(() {
+                            file = files;
+                          });
+                        },
+                        splashColor: Colors.white,
+                        child: Icon(Icons.add_a_photo, color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Container(color: Colors.black, width: 200, height: 600),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Container(color: Colors.black, width: 200, height: 600),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Container(color: Colors.black, width: 200, height: 600),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Container(color: Colors.black, width: 200, height: 600),
-                )
+                TextButton(onPressed: () {}, child: Text("Post"))
               ],
             ),
           )),
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -39,7 +39,7 @@ class _HomeState extends ConsumerState<Home> {
           ],
         ),
       )),
-      FeedScreen(),
+      CreateClips(),
     ];
   }
 
